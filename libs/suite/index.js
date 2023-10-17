8c077a9d433fd831f4d5a802834912a03be910da
pruebatgr2023 <145792510+pruebatgr2023@users.noreply.github.com>
2023-10-17 19:03:12 -0300

    b

---
+++ libs/suite/index.js
@@ -0,0 +1,4 @@
+"use strict";
+// ./node_modules/cucumber/bin/cucumber-js suite/features/basic.feature --require suite/support --require suite/steps --require suite/pages --world-parameters '{"executionID":"hola"}' --format json:cucumber_report.json
+// ./node_modules/cucumber/bin/cucumber-js suite/features/TGRPA-311.feature --require suite/support --require suite/steps --require suite/pages --world-parameters '{"executionID":"hola", "bucketKey": "pazthor"}' --format json:cucumber_report.json
+//# sourceMappingURL=index.js.map
